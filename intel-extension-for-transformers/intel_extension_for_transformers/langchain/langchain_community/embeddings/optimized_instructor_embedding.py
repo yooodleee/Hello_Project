9e63d962a0dac6db19d65751be128ddad506d03e
yooodleee <accia25@naver.com>
2025-02-20 12:39:22 +0900

    prepare

---
+++ intel-extension-for-transformers/intel_extension_for_transformers/langchain/langchain_community/embeddings/optimized_instructor_embedding.py
@@ -0,0 +1,22 @@
+
+import os
+import json
+import torch
+import logging
+from collections import OrderedDict
+
+
+from intel_extension_for_transformers.transformers import OptimizeModel
+from intel_extension_for_transformers.transformers.utils.utility import LazyImport
+from transformers import T5Config, MT5Config
+
+
+from typing import Union, Optional
+from .optimized_sentence_transformers import OptimizedTransformer
+
+
+
+sentence_transformers = LazyImport("sentence_transformers")
+InstructorEmbedding = LazyImport("InstructorEmbedding")
+
+logger = logging.getLogger(__name__)
