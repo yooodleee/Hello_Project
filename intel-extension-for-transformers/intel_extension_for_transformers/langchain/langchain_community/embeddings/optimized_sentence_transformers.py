86743db7ee0bf689781868ff59e7ea1cbef0331e
yooodleee <accia25@naver.com>
2025-02-20 14:10:12 +0900

    forward
    returns token_embeddings, cls_token

--- intel-extension-for-transformers/intel_extension_for_transformers/langchain/langchain_community/embeddings/optimized_sentence_transformers.py
+++ intel-extension-for-transformers/intel_extension_for_transformers/langchain/langchain_community/embeddings/optimized_sentence_transformers.py
@@ -44,4 +44,42 @@ class OptimizedTransformer(sentence_transformers.models.Transformer):
             setattr(self.auto_model, "config", config)
     
 
-    
+    def forward(self, features):
+        """Returns token_embeddings, cls_token."""
+        trans_features = {
+            "input_ids": features["input_ids"],
+            "attention_mask": features["attention_mask"],
+        }
+
+        if "token_type_ids" in features:
+            trans_features["token_type_ids"] = features["token_type_ids"]
+        
+        if isinstance(self.auto_model, torch.jit.ScriptModule):
+            output_states = self.auto_model(**trans_features)
+            if isinstance(output_states, dict):
+                output_states = tuple(output_states.values())
+            
+            output_tokens = output_states[0]
+        else:
+            output_states = self.auto_model(**trans_features, return_dict=False)
+            output_tokens = output_states[0]
+        
+        features.update(
+            {
+                "token_embeddings": output_tokens,
+                "attention_mask": features["attention_mask"],
+            }
+        )
+
+        if self.auto_model.config.output_hidden_states:
+            all_layer_idx = 2
+            if len(output_states) < 3:
+                all_layer_idx = 1
+            
+            hidden_states = output_states[all_layer_idx]
+            features.update({"all_layer_embeddings": hidden_states})
+        
+        return features
+
+
+
