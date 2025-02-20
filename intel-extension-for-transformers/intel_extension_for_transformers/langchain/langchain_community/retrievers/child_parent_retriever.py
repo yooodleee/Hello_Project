23ff8979ba5fa91ac13f28ea75a38d9e3b548d1f
yooodleee <accia25@naver.com>
2025-02-20 14:56:55 +0900

    add ChildParentRetriever
    retrieve from a set of multiple embeddings for the same doc.

--- intel-extension-for-transformers/intel_extension_for_transformers/langchain/langchain_community/retrievers/child_parent_retriever.py
+++ intel-extension-for-transformers/intel_extension_for_transformers/langchain/langchain_community/retrievers/child_parent_retriever.py
@@ -21,3 +21,58 @@ class SearchType(str, Enum):
 
 
 
+class ChildParentRetriever(BaseRetriever):
+    """Retrieve from a set of multiple embeddings for the same doc."""
+    vectorstore: VectorStore
+    parentstore: VectorStore
+    id_key: str = "doc_id"
+    search_kwargs: dict = Field(default_factory=dict)
+    """Keyword args to pass to the search func."""
+    search_type: SearchType = SearchType.similarity
+    """Type of search to perform (similarty / mmr)"""
+
+
+    def _get_relevant_documents(
+            self,
+            query: str, 
+            *,
+            run_manager: CallbackManagerForRetrieverRun,
+    ) -> List[Document]:
+        """
+        Get docs relevant to a query.
+
+
+        Args
+        ----------------
+            query: String to find relevant docs for
+            run_manager: The callbacks handler to use
+
+
+        Returns
+        -----------------
+            The concatation of the retrieved docs and the link
+        """
+        ids = []
+        results = []
+
+        if self.search_type == SearchType.mmr:
+            sub_docs = self.vectorstore.max_marginal_relevance_search(
+                query, **self.search_kwargs
+            )
+        else:
+            sub_docs = self.vectorstore.similarity_search(query, **self.search_kwargs)
+        
+
+        for d in sub_docs:
+            if d.metadata["identify_id"] not in ids:
+                ids.append(d.metadata['identify_id'])
+        
+
+        retrieved_documents = self.parentstore.get(ids)
+        for i in range(len(retrieved_documents['ids'])):
+            metadata = retrieved_documents['metadatas'][i]
+            context = retrieved_documents['documents'][i]
+            instance = Document(page_content=context, metadata=metadata)
+            results.append(instance)
+        
+        return results
