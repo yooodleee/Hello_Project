ac93eefa6a349a5096fd221a3172b83a1d009946
yooodleee <accia25@naver.com>
2025-02-20 12:00:06 +0900

    code
    compute doc embeddings using a huggingface transformer model.

--- intel-extension-for-transformers/intel_extension_for_transformers/langchain/langchain_community/embeddings/embeddings.py
+++ intel-extension-for-transformers/intel_extension_for_transformers/langchain/langchain_community/embeddings/embeddings.py
@@ -89,4 +89,49 @@ class HuggingFaceEmbeddings(
 
         extra = langchain_core.pydantic_v1.Extra.forbid
     
+
+    def embed_documents(self, texts: List[str]) -> List[List[float]]:
+        """
+        Compute doc embeddings using a HuggingFace transformer model.
+
+
+        Args
+        --------------------
+            texts: The list of texts to embed.
+
+        Returns
+        ---------------
+            List of embeddings, one for each text.
+        """
+        import sentence_transformers
+
+        texts = list(map(lambda x: x.replace("\n", " "), texts))
+        if self.multi_process:  # pragma: no cover
+            pool = self.client.start_multi_process_pool()
+            embeddings = self.client.encode_multi_process(texts, pool)
+            sentence_transformers.SentenceTransformer.stop_multi_process_pool(pool)
+        
+        else:
+            embeddings = self.client.encode(texts, **self.encode_kwargs)
+        
+        return embeddings.tolist()
     
+
+    def embed_query(self, text: str) -> List[float]:
+        """
+        Compute query embeddings using a HuggingFace transformer model.
+
+
+        Args
+        ----------------
+            text: The text to embed.
+
+        Returns
+        ----------------
+            Embeddings for the text.
+        """
+        
+        return self.embed_documents([text])[0]
+
+
+
