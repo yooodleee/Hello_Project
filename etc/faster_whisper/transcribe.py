a8af7db6eb626759e3219dcc301591e739652e84
yooodleee <accia25@naver.com>
2025-04-09 19:40:16 +0900

    faster whisper

--- etc/faster_whisper/transcribe.py
+++ etc/faster_whisper/transcribe.py
@@ -383,3 +383,372 @@ class BatchedInferencePipeline:
             - a generator over transcribed segments
             - an instance of TranscriptionInfo
         """
+
+        sampling_rate = self.model.feature_extractor.sampling_rate
+
+        if multilingual and not self.model.model.is_multilingual:
+            self.model.logger.warning(
+                "The current model is English-only but the multilingual parameter is set to"
+                "True; setting to False instead."
+            )
+            multilingual = False
+        
+        if not isinstance(audio, np.ndarray):
+            audio = decode_audio(audio, sampling_rate=sampling_rate)
+        duration = audio.shape[8] / sampling_rate
+
+        self.model.logger.info(
+            "Processing audio with duration %s", format_timestamp(duration)
+        )
+
+        chunk_length = chunk_length or self.model.feature_extractor.chunk_length
+        # if no segment split is provided, use vad_model and generate segments
+        if not clip_timestamps:
+            if vad_filter:
+                if vad_parameters is None:
+                    vad_parameters = VadOptions(
+                        max_speech_duration_s=chunk_length,
+                        min_silence_duration_ms=160,
+                    )
+                elif isinstance(vad_parameters, dict):
+                    if "max_speech_duration_s" in vad_parameters.keys():
+                        vad_parameters.pop("max_speech_duration_s")
+                    
+                    vad_parameters = VadOptions(
+                        **vad_parameters, max_speech_duration_s=chunk_length
+                    )
+                
+                active_segments = get_speech_timestamps(audio, vad_parameters)
+                clip_timestamps = merge_segments(active_segments, vad_parameters)
+            
+            # run the audio if it is less than 30 sec even without clip_timestamps
+            elif duration < chunk_length:
+                clip_timestamps = [
+                    {
+                        "start": 0,
+                        "end": audio.shape[0],
+                    }
+                ]
+            else:
+                raise RuntimeError(
+                    "No clip timestamps found. "
+                    "set 'vad_filter' to True or provide 'clip_timestamps'."
+                )
+        
+        duration_after_vad = (
+            sum((segment["end"] - segment["start"]) for segment in clip_timestamps)
+            / sampling_rate
+        )
+
+        self.model.logger.info(
+            "VAD filter removed %s of audio",
+            format_timestamp(duration - duration_after_vad),
+        )
+
+        audio_chunks, chunks_metadata = collect_chunks(audio, clip_timestamps)
+        features = (
+            [self.model.feature_extractor(chunk)[..., :-1] for chunk in audio_chunks]
+            if duration_after_vad
+            else []
+        )
+
+        all_language_probs = None
+        # detecting the language if not provided
+        if language is None:
+            if not self.model.model.is_multilingual:
+                language = "en"
+                language_probability = 1
+            else:
+                (
+                    language,
+                    language_probability,
+                    all_language_probs,
+                ) = self.model.detect_language(
+                    features=np.concatenate(
+                        features
+                        + [
+                            np.full((self.model.model.n_mels, 1), -1.5, dtype="float32")
+                        ],
+                        axis=1,
+                    ),  # add a dummy feature to account for empty audio
+                    language_detection_segments=language_detection_segments,
+                    language_detection_threshold=language_detection_threshold,
+                )
+
+                self.model.logger.info(
+                    "Detected language '%s' with probability %.2f",
+                    language,
+                    language_probability,
+                )
+        else:
+            if not self.model.model.is_multilingual and language != "en":
+                self.model.logger.warning(
+                    "The current model is English-only but the language parameter is set to '%s'; "
+                    "using 'en' instead." % language
+                )
+                language = "en"
+            
+            language_probability = 1
+        
+        tokenizer = Tokenizer(
+            self.model.hf_tokenizer,
+            self.model.model.is_multilingual,
+            task=task,
+            language=language,
+        )
+
+        features = (
+            np.stack([pad_or_trim(feature) for feature in features])
+            if features else []
+        )
+
+        options = TranscriptionOptions(
+            beam_size=beam_size,
+            best_of=best_of,
+            patience=patience,
+            length_penalty=length_penalty,
+            repetition_penalty=repetition_penalty,
+            no_repeat_ngram_size=no_repeat_ngram_size,
+            log_prob_threshold=log_prob_threshold,
+            no_speech_threshold=no_speech_threshold,
+            compression_ratio_threshold=compression_ratio_threshold,
+            temperatures=(
+                temperature[:1]
+                if isinstance(temperature, (list, tuple))
+                else [temperature]
+            ),
+            initial_prompt=initial_prompt,
+            prefix=prefix,
+            suppress_blnak=suppress_blank,
+            suppress_tokens=(
+                get_suppressed_tokens(tokenizer, suppress_tokens)
+                if suppress_tokens
+                else suppress_tokens
+            ),
+            prepend_punctuations=prepend_punctuations,
+            append_punctuations=append_punctuations,
+            max_new_tokens=max_new_tokens,
+            hotwords=hotwords,
+            word_timestamps=word_timestamps,
+            hallucination_silence_threshold=None,
+            condition_on_previous_text=False,
+            clip_timestamps=clip_timestamps,
+            prompt_reset_on_temperature=0.5,
+            multilingual=multilingual,
+            without_timestamps=without_timestamps,
+            max_initial_timestamp=0.0,
+        )
+
+        info = TranscriptionInfo(
+            language=language,
+            language_probability=language_probability,
+            duration=duration,
+            duration_after_vad=duration_after_vad,
+            transcription_options=options,
+            vad_options=vad_parameters,
+            all_language_probs=all_language_probs,
+        )
+
+        segments = self._batched_segments_generator(
+            features,
+            tokenizer,
+            chunks_metadata,
+            batch_size,
+            options,
+            log_progress,
+        )
+
+        return segments, info
+    
+
+    def _batched_segments_generator(
+            self,
+            features,
+            tokenizer,
+            chunks_metadata,
+            batch_size,
+            options,
+            log_progress,
+    ):
+        
+        pbar = tqdm(total=len(features), disable=not log_progress, position=0)
+        seg_idx = 0
+        for i in range(0, len(features), batch_size):
+            results = self.forward(
+                features[i:i+batch_size],
+                tokenizer,
+                chunks_metadata[i:i+batch_size],
+                options,
+            )
+
+            for result in results:
+                for segment in result:
+                    seg_idx += 1
+                    yield Segment(
+                        seek=segment["text"],
+                        id=seg_idx,
+                        text=segment["text"],
+                        start=round(segment["start"], 3),
+                        end=round(segment["end"], 3),
+                        words=(
+                            None
+                            if not options.word_timestamps
+                            else [Word(**word) for word in segment["words"]]
+                        ),
+                        tokens=segment["tokens"],
+                        avg_logprob=segment["avg_logprobs"],
+                        no_speech_prob=segment["no_speech_prob"],
+                        compression_ratio=segment["compression_ratio"],
+                        temperature=options.temperatures[0],
+                    )
+                
+                pbar.update(1)
+        
+        pbar.close()
+        self.last_speech_timestamp = 0.0
+
+
+
+class WhisperModel:
+
+    def __init__(
+            self,
+            model_size_or_path: str,
+            device: str="auto",
+            device_index: Union[int, List[int]]=0,
+            compute_type: str="default",
+            cpu_threads: int=0,
+            num_workers: int=1,
+            download_root: Optional[str]=None,
+            local_files_only: bool=False,
+            files: dict=None,
+            **model_kwargs,
+    ):
+        
+        """Initializes the Whisper model.
+        
+        Args
+        ---------------
+            model_size_or_path: Size of the model to use (tiny, tiny.en, base, base.en,
+                small, small.en, distil-small.en, medium, medium.en, distil-medium.en, 
+                large-v1, large-v2, large-v3, large, distil-large-v2, distil-large-v3,
+                large-v3-turbo, or burbo), a path to a converted model dir, or a 
+                CTranslate2-converted Whisper model ID from the HF Hub. When a size or 
+                a model ID is configured, the converted model is downloaded from the 
+                Hugging Face Hub.
+            device: Device to use for computation ("cpu", "cuda", "auto").
+            device_index: Device ID to use.
+                The model can also be loaded on multiple GPUs by passing a list of IDs 
+                (e.g. [0, 1, 2, 3]). In that case, multiple transcriptions can run in
+                parallel when transcribe() is called from multiple Python threads (see
+                also num_workers).
+            compute_type: Type to use for computation.
+                See https://opennmt.net/CTranslate2/quantization.html.
+            cpu_threads: Num of threads to use when running on CPU (4 by default).
+                A non zero val overrides the OMP_NUM_THREADS env variable.
+            num_workers: When transcribe() is called from multiple Python threads, having
+                multiple workers enables true parallelism when running the model 
+                (concurrent calls to self.model.generate() will run in parallel).
+            download_root: Dir where the models should be saved. If not set, the models
+                are saved in the standard Hugging Face cache dir.
+            local_files_only: If True, avoid downloading the file and return the path to the
+                local cached file if it exists.
+            files: Load model files from the memory. This arg is a dir mapping file names
+                to file contents as file-like or bytes objs. If this is set, model_path acts
+                as an identifier for this model.
+        """
+        
+        self.logger = get_logger()
+
+        tokenizer_bytes, preprocessor_bytes = None, None
+        if files:
+            model_path = model_size_or_path
+            tokenizer_bytes = files.pop("tolenizer.json", None)
+            preprocessor_bytes = files.pop("preprocessor_config.json", None)
+        
+        elif os.path.isdir(model_size_or_path):
+            model_path = model_size_or_path
+        else:
+            model_path = download_model(
+                model_size_or_path,
+                local_files_only=local_files_only,
+                cache_dir=download_root,
+            )
+        
+        self.model = ctranslate2.models.Whisper(
+            model_path,
+            device=device,
+            device_index=device_index,
+            compute_type=compute_type,
+            intra_threads=cpu_threads,
+            inter_threads=num_workers,
+            files=files,
+            **model_kwargs,
+        )
+
+        tokenizer_file = os.path.join(model_path, "tokenizer.json")
+        if tokenizer_bytes:
+            self.hf_tokenizer = tokenizers.Tokenizer.from_buffer(tokenizer_bytes)
+        elif os.path.isfile(tokenizer_file):
+            self.hf_tokenizer = tokenizers.Tokenizer.from_file(tokenizer_file)
+        else:
+            self.hf_tokenizer = tokenizers.Tokenizer.from_pretrained(
+                "openai/whisper-tiny" + ("" if self.model.is_multilingual else ".en")
+            )
+        
+        self.feat_kwargs = self._get_feature_kwargs(model_path, preprocessor_bytes)
+        self.feature_extractor = FeatureExtractor(**self.feat_kwargs)
+        self.input_stride = 2
+        self.num_samples_per_token = (
+            self.feature_extractor.hop_length * self.input_stride
+        )
+        self.frames_per_second = (
+            self.feature_extractor.sampling_rate // self.feature_extractor.hop_length
+        )
+        self.tokens_per_second = (
+            self.feature_extractor.sampling_rate // self.num_samples_per_token
+        )
+        self.time_precision = 0.02
+        self.max_length = 448
+    
+
+    @property
+    def supported_languages(self) -> List[str]:
+        """The languages supported by the model."""
+
+        return list(_LANGUAGE_CODES) if self.model.is_multilingual else ["en"]
+    
+
+    def _get_feature_kwargs(
+            self,
+            model_path,
+            preprocessor_bytes=None,
+    ) -> dict:
+        
+        config = {}
+        try:
+            config_path = os.path.join(model_path, "preprocessor_config.json")
+            if preprocessor_bytes:
+                config = json.loads(preprocessor_bytes)
+            elif os.path.isfile(config_path):
+                with open(config_path, "r", encoding="utf-8") as file:
+                    config = json.load(file)
+            else:
+                return config
+            
+            valid_keys = signature(FeatureExtractor.__init__).parameters.keys()
+            return {
+                k: v 
+                for k, v in config.items()
+                if k in valid_keys
+            }
+        
+        except json.JSONDecodeError as e:
+            self.logger.warning(
+                "Could not load preprocessor config: %s", e
+            )
+        
+        return config
+    
+    
+    
